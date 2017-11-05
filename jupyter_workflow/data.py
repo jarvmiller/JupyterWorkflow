3006427760b15c2a3d8e4876c4a8299be3ca5623
Jarvis Miller <jarvm@Jarviss-MacBook-Pro.local>
2017-11-04 20:29:21 -0400

    created python package

---
+++ jupyter_workflow/data.py
@@ -0,0 +1,19 @@
+from urllib.request import urlretrieve
+import os
+import pandas as pd
+FREMONT_URL = 'https://data.seattle.gov/api/views/65db-xm6k/rows.csv?accessType=DOWNLOAD'
+
+def get_fremont_data(filename='Fremont.csv', url=FREMONT_URL, force_download=False):
+    """
+    Download and cache fremont data
+    """
+    if force_download or not os.path.exists(filename):
+        print("downloading data")
+        urlretrieve(URL, filename)
+    data = pd.read_csv('Fremont.csv', index_col='Date',
+                       parse_dates=True)
+    data.columns = ['West', 'East']
+    data['Total'] = data['West'] + data['East']
+    return data
+
+
